ef87e7f37f17c417c30550bb17fc90a30968c772
20400092428 <molinagomezgonzalo@gmail.com>
2024-11-19 10:33:10 -0300

    feat: tests updated

--- tests/testDebitCards.py
+++ tests/testDebitCards.py
@@ -1,17 +1,47 @@
+import pytest
 from unittest import mock
 import os
 import models.debitCards as dc
+import logging
 
-def test_getOneDebitCard_withFakeData():
-    fake_data_path = "tests/mockDataTests/mockDebitCards.json"
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+fake_data_path = "tests/mockDataTests/mockDebitCards.json"
     
-    assert os.path.exists(fake_data_path), f"El archivo {fake_data_path} no existe."
+assert os.path.exists(fake_data_path), f"El archivo {fake_data_path} no existe."
 
+@pytest.fixture(autouse=True)
+def mock_debit_card_path():
     with mock.patch('models.debitCards.DEBIT_CARD_PATH', fake_data_path):
-        username = "user1"
-        result = dc.getOneDebitCard(username)
-        assert result is not False, "El usuario debería tener una tarjeta."
-        assert result["card_number"] == "1234567890123456", "Número de tarjeta incorrecto."
-        username = "nonexistentuser"
-        result = dc.getOneDebitCard(username)
-        assert result is False, "El usuario no debería tener una tarjeta."
+        yield
+
+def test_getOneDebitCard_userExists():
+    username = "user1"
+    result = dc.getOneDebitCard(username)
+    assert result is not False, "El usuario debería tener una tarjeta."
+    assert result["card_number"] == "1234567890123456", "Número de tarjeta incorrecto."
+    logger.info("\n 1-Test de usuario existente pasó exitosamente.")
+
+def test_getOneDebitCard_userDoesNotExist():
+    username = "nonexistentuser"
+    result = dc.getOneDebitCard(username)
+    assert result is False, "El usuario no debería tener una tarjeta."
+    logger.info("\n 2-Test de usuario no existente pasó exitosamente.")
+
+def test_getOneDebitCard_intUsername():
+    username = 1
+    result = dc.getOneDebitCard(username)
+    assert result is False, "La función debería devolver False cuando el nombre de usuario no es un string"
+    logger.info("\n 3-Test de nombre de usuario int pasó exitosamente.")
+
+def test_getOneDebitCard_noneUsername():
+    username = None
+    result = dc.getOneDebitCard(username)
+    assert result is False, "La función debería devolver False cuando el nombre de usuario es None."
+    logger.info("\n 4-Test de nombre de usuario None pasó exitosamente.")
+
